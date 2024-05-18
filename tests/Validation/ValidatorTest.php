d249b10515513a66d087f414299773bfeffb2a4a
Marcos Busto <bustosuarezmarcos@gmail.com>
2024-05-18 10:35:27 +0200

    ValidatorTest

---
+++ tests/Validation/ValidatorTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Prism\Tests\Validation;
+
+use Prism\Validation\Exceptions\ValidationException;
+use Prism\Validation\Rule;
+use Prism\Validation\Rules\Email;
+use Prism\Validation\Rules\LessThan;
+use Prism\Validation\Rules\Number;
+use Prism\Validation\Rules\Required;
+use Prism\Validation\Rules\RequiredWith;
+use Prism\Validation\Validator;
+use PHPUnit\Framework\TestCase;
+
+class ValidatorTest extends TestCase
+{
+    public function test_basic_validation_passes()
+    {
+        $data = [
+            "email" => "test@test.com",
+            "other" => 2,
+            "num" => 3,
+            "foo" => 5,
+            "bar" => 4,
+        ];
+
+        $rules = [
+            "email" => new Email(),
+            "other" => new Required(),
+            "num" => new Number(),
+        ];
+
+        $expected = [
+            "email" => "test@test.com",
+            "other" => 2,
+            "num" => 3,
+        ];
+
+        $v = new Validator($data);
+
+        $this->assertEquals($expected, $v->validate($rules));
+    }
+
+    public function test_throws_validation_exception_on_invalid_data()
+    {
+        $this->expectException(ValidationException::class);
+
+        $v = new Validator(["test" => "test"]);
+
+        $v->validate(["test" => new Number()]);
+    }
+
+    /**
+     * @depends test_basic_validation_passes
+     */
+    public function test_multiple_rules_validation()
+    {
+        $data = ["age" => 20, "num" => 3, "foo" => 5];
+        $rules = [
+            "age" => new LessThan(100),
+            "num" => [new RequiredWith("age"), new Number()],
+        ];
+        $expected = ["age" => 20, "num" => 3];
+        $v = new Validator($data);
+
+        $this->assertEquals($expected, $v->validate($rules));
+    }
+}
